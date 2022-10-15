6cdd46c7132830efad6102864df39da0d4425935
Topo81 <103972679+Topo81@users.noreply.github.com>
2022-10-15 12:28:41 -0300

    Create script.js

---
+++ Mi-Proyecto-Entregable1/script.js
@@ -0,0 +1,66 @@
+let destino
+let pago
+do {
+     destino =Number( prompt("¿A que destino desea viajar?       1-Buenos Aires    2-Cordoba"))
+        if (((destino < 1) || (destino > 2))) {
+        alert("ERROR: INGRESE UN DESTINO VALIDO")
+        }
+    
+    } while ((destino < 1) || (destino > 2))  
+
+do {
+    pago = Number(prompt("Introduce metodo de pago   1-Efectivo     2-Tarjeta de credito (15% de recargo) "));
+    if (((pago < 1) || (pago > 2))) {
+        alert("ERROR:INGRESE UN MODO DE PAGO VALIDO")
+    }
+} while ((pago < 1) || (pago > 2)) ;
+
+const buenosaires= 3500  // 
+const cordoba= 2500
+const efectivo= 1
+const tarjeta= (1+(15/100))
+
+destino= procesarDestino(destino)
+pago=  procesarPago(pago)  
+console.log(destino,pago)
+costo(destino,pago)  
+
+function procesarDestino(destino){
+    switch(destino){
+      case 1:
+            destino=buenosaires
+            console.log("prueba")
+        break
+      case 2:
+            destino=cordoba
+            console.log("030303456lalalalal")
+        break
+    default:
+          
+         alert('no es un destino válido') //se supone que esta aca 
+    break
+        
+      }
+      return(destino)
+} 
+
+function procesarPago(pago){
+    switch(pago){
+    case 1:
+     pago= efectivo
+    break
+    case 2:
+     pago= tarjeta
+    break
+        default:
+     alert('no es un metodo de pago válido')
+    break
+     } 
+     return(pago)
+}  
+
+
+function costo(destino,pago){
+    alert("Su viaje cuesta " + (destino * pago).toFixed(2))
+}
+
